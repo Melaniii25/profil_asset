4c9754b49ffd809c0cfec2acf2b090418ac254a6
Melaniii25 <melanisulistyowati25@gmail.com>
2025-05-02 22:06:46 +0700

    menambah gambar

--- lib/profil_scren.dart
+++ lib/profil_scren.dart
@@ -1,54 +1,44 @@
 import 'package:flutter/material.dart';
 
-class MahasiswaProfil extends StatelessWidget {
-  const MahasiswaProfil({super.key});
-
+class ProfilScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Profil Saya'),
-        centerTitle: true,
-      ),
+        title: Text('Halaman Profil', style: TextStyle(fontSize: 18)),
+        backgroundColor: Color.fromARGB(255, 162, 202, 214),
+      ), 
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            // Foto Profil
-            CircleAvatar(
-              radius: 60,
-              backgroundImage: AssetImage('assets/images/pikachu.png'),
-            ),
-            const SizedBox(height: 20),
-            // Card Data Diri
-            Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16),
-              ),
-              elevation: 4,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Text(
-                      'Nama: Pikachu',
-                      style: TextStyle(fontSize: 18),
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      'Email: pikachu@pokemon.com',
-                      style: TextStyle(fontSize: 18),
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      'Telepon: 0812-3456-7890',
-                      style: TextStyle(fontSize: 18),
-                    ),
-                  ],
-                ),
+            Center(
+              child: CircleAvatar(
+                radius: 100,
+                backgroundImage: AssetImage('image/melani.jpeg'),
               ),
             ),
+        SizedBox(height: 16),
+        Text('Sulistyowati Dwiningsih Melani', 
+        style: TextStyle(fontFamily: 'ComicRelief', fontSize:24, fontWeight: FontWeight.bold),
+        ),
+         Divider(height: 15),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.insights),
+            title: Text('NIM', style: TextStyle(fontFamily: 'ComicRelief', fontSize: 15, fontWeight: FontWeight.bold),),
+            subtitle: Text('220102028'),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.school),
+            title: Text('Program Studi', style: TextStyle(fontFamily: 'ComicRelief', fontSize: 15, fontWeight: FontWeight.bold)), 
+            subtitle: Text(
+              'D4 Teknologi Rekayasa Perangkat Lunak', style: TextStyle(fontFamily: 'ComicRelief-Reguler'),
+            ),
+          ),
+        ),
           ],
         ),
       ),
